6dcc8cb2fa1ccf2dbbf7c1ecf8a6c0d2811fb8c4
Zaxy_liu <3310828534@qq.com>
2026-03-26 14:22:53 +0800

    Finish functions for move_tests::ret_1_given_cmd_m_facing_w

--- executor/src/executor.rs
+++ executor/src/executor.rs
@@ -36,6 +36,7 @@ impl Executor {
                 'M' => match self.pose.heading {
                     'E' => self.pose.x += 1,
                     'S' => self.pose.y -= 1,
+                    'W' => self.pose.x -= 1,
                     _ => (),
                 },
                 _ => (),
