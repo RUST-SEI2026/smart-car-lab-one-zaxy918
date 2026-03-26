0c953d179e870b081096c65ec78d6b0ca3a24d01
Zaxy_liu <3310828534@qq.com>
2026-03-26 14:19:30 +0800

    finish test case: ret_1_given_cmd_m_facing_s

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -10,4 +10,10 @@ mod move_tests {
         let expected_pose = Pose::new(1, 0, 'E');
         assert_eq!(ececutor.query(), expected_pose);
     }
+    #[test]
+    fn ret_1_given_cmd_m_facing_s() {
+        let mut executor = Executor::with_pose(Pose::new(0, 0, 'S'));
+        executor.execute("M");
+        assert_eq!(executor.query(), Pose::new(0, -1, 'S'));
+    }
 }
